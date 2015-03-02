3cc519152e1b5721d3647168f421d9a066c375f0
Ian Gawronski <iangawronski@gmail.com>
2015-03-02 14:39:15 -0500

    added a little structure

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,24 @@
+class ToDosController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
+end
---
+++ app/models/todo.rb
@@ -0,0 +1,4 @@
+class ToDo <ActiveRecord::Base
+  belongs_to :user
+  belongs_to :list
+end
